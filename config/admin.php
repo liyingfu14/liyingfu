43caf17d728b02f119202e45c783891a80632733
liyingfu <1145612834@qq.com>
2018-07-05 22:56:17 +0800

    1

--- config/admin.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    //获取上传token地址
-//     'upload_token_url'  =>  'http://upload.com:8001/index.php/api/upload/token',
-    'upload_token_url' => 'http://upload.4cgame.com/index.php/api/upload/token',
-    'upload_p' => '4cgame_admin_app',
-];
